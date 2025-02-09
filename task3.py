1af4e1086556618e84d1c4d739add80580fd6b87
geniusokorro <geniusokorro@yahoo.co.uk>
2025-02-09 14:23:33 +0000

    A program that prints a character for the length of a declared range.

---
+++ task3.py
@@ -0,0 +1,7 @@
+#Printing * for each item in a range
+print ("Welcome!! This program prints Asterisk on the screen") 
+
+asterisk = "" #Creating and empty variable that Initializes asterick
+for each in range(0,10):
+    asterisk = asterisk + "*" #Increment
+    print (asterisk) #Prints each instance of asterisk 
