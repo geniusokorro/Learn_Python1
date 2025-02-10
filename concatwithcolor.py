e1e5c5e4046e82a2afc9b0d6956ec4b03dd525d0
geniusokorro <geniusokorro@yahoo.co.uk>
2025-02-10 18:31:35 +0000

    Python concatwithcolor shows how print and input statements work with a flavour of colouring the text.

---
+++ concatwithcolor.py
@@ -0,0 +1,9 @@
+print("Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!")
+
+name =input ("What is your name?")
+enemy = input ("What is your worst enemy’s name?")
+superPower = input ("What is your superpower?")
+location = input ("Where do you live?")
+favFood = input ("What is your favorite food?")
+
+print (f"Hello \033[31m{name}\033[0m! Your ability to {superPower} will make sure you never have to look at {enemy} again. Go eat {favFood} as you walk down the streets of {location} and use {superPower} for good and not evil!")
