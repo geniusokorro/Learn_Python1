ced4096f6adff0633b31870ed1c7e443d1c2f569
geniusokorro <geniusokorro@yahoo.co.uk>
2025-02-12 13:29:11 +0000

    My First Code: #Code to Accept input and Print

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#Code to Accept input and Print
+print ("Welcome to my First HyperionDev Code \n")
+name_user = input ("Kindly enter your name: ") #Collect name from keyboard
+print ("This user's name is " + name_user)
+age_user = input ("Kindly enter your age in numbers: ") #Collect Age from keyboard
+print ("This user's age is " + age_user + "\nHello World!")
+#Using the format method to collect variable content especially for int without casting to string.
+print (f"This user's name is  {name_user} \nThis user's age is {age_user} \nHello World!")
+
