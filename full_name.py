0237323734802ce10810d6d243957c3e63f00b64
geniusokorro <geniusokorro@yahoo.co.uk>
2025-02-14 12:51:17 +0000

    Using len to confirm that an input is not empty or longer than required.

---
+++ full_name.py
@@ -0,0 +1,14 @@
+#Validate that each user has a full name
+
+full_name = input ("Enter your full name (first and last names): ")
+
+if len(full_name) == 0:
+    print ("You haven't enter anything. Please enter your full name")
+
+elif len(full_name)<=4:
+    print ("You have entered less than 4 characters. Please make sure that you have entered your name and surname.")
+
+elif len(full_name) >= 25:
+    print ("You have entered more than 25 characters. Please make sure that you have only entered your full name.")
+else:
+    print ("Thank you for entering your name.")
