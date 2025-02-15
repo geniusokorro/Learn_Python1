7f1da56284c636ef5e45d57f929f1a76b14ba8da
geniusokorro <geniusokorro@yahoo.co.uk>
2025-02-15 21:22:06 +0000

    using print and input statements

---
+++ menu.py
@@ -0,0 +1,17 @@
+#Order 3 favourite food items
+
+print ("Welcome to my Second HyperionDev Code \n")
+#User details
+user_name = input ("Kindly enter your name: ")
+print ("Welcome to our ordering system \nHello " + user_name )
+#Collect user input
+item1 = input ("Enter your 1st favourite food item: ") 
+item2 = input ("Enter your 2nd favourite food item: ") 
+item3 = input ("Enter your 3rd favourite food item: ") 
+#Print Statements
+print ("\nOrder Confirmation!! \nBelow is a list of your order:")
+print ("First items: " + item1)
+print ("First items: " + item2)
+print ("First items: " + item3)
+print ("Thank you for ordering your favourite food items with us!")
+
